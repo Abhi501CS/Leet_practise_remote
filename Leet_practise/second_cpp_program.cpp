6a516790714bf9e9b3fecb301a6f9528b32feb49
Abhimaccode <abhishiktvenkata@gmail.com>
2022-04-24 09:23:35 +0530

    my second program

---
+++ Leet_practise/second_cpp_program.cpp
@@ -0,0 +1,14 @@
+//
+//  second_cpp_program.cpp
+//  Leet_practise
+//
+//  Created by abhihsikt vv on 24/04/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello Abhishikt"<<endl;
+}
